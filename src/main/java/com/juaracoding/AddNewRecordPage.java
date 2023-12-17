74f76d82ac77d3c37582e72f0434c7b27e2f8f87
Ikrar Bagaskara <ikrarb95@gmail.com>
2023-12-17 15:30:30 +0700

    add new function to close the alert after splash screen

--- src/main/java/com/juaracoding/AddNewRecordPage.java
+++ src/main/java/com/juaracoding/AddNewRecordPage.java
@@ -3,17 +3,28 @@ package com.juaracoding;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.pagefactory.AndroidFindBy;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.support.FindBy;
 
 public class AddNewRecordPage extends PageBase{
     public AddNewRecordPage(AppiumDriver appiumDriver) {
         super(appiumDriver);
     }
+    @FindBy(id = "android:id/button2")
+    private MobileElement closeButtonAlerts;
 
     @FindBy(id = "fabMenu")
     static MobileElement addNewRecord;
 
     public void clickAddNewRecord(){
         click(addNewRecord);
+    }
+
+    public void clickCloseButtonAlerts(){
+        try{
+            click(closeButtonAlerts);
+        } catch (NoSuchElementException e) {
+            System.out.println("Notifikasi sudah di setujui.");
+        }
     }
 }
